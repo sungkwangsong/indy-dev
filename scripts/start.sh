287fc5d98aa9d435819566327436a0d84c5f41ac
Michael Boyd <michaeldavisboyd@gmail.com>
2019-05-23 15:21:10 +0800

    added root access to docker
    Signed-off-by: Michael Boyd <michaeldavisboyd@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,21 +5,22 @@ if [ "$#" -eq 0 ];
         if [[ "$(docker ps -a -f status=running | grep indy_pool)" ]];
             then
                 echo "entering " $(pwd) "directory"
-                docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
+                docker run -u root -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
             else
                 echo "entering " $(pwd) "directory"
-                docker run -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
-                docker run -it --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
+                docker run -u root -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
+                docker run -it -u root --net=host -p 127.0.0.1:8080:8080 -v $(pwd):/home/indy indy_dev
         fi;
     else
         if [[ "$(docker ps -a -f status=running | grep indy_pool)" ]];
             then
                 echo "entering $1 directory"
-                docker run -it --net=host -p 127.0.0.1:8080:8080 -v "$1":/home/indy indy_dev
+                docker run -u root -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
+                docker run -u root -it --net=host -p 127.0.0.1:8080:8080 -v "$1":/home/indy indy_dev
             else
                 echo "entering $1 directory"
-                docker run -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
-                docker run -it --net=host -p 127.0.0.1:8080:8080 -v "$1":/home/indy indy_dev
+                docker run -u root -itd --net=host -p 127.0.0.1:9701-9708:9701-9708 indy_dev_pool
+                docker run -u root -it --net=host -p 127.0.0.1:8080:8080 -v "$1":/home/indy indy_dev
         fi;
 fi;
 
